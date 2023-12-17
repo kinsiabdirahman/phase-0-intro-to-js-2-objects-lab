80ae45116d4a03a6dd71fd1a97bb362c530671c5
kinsiabdirahman <kinsiabdirahman@outlook.com>
2023-12-17 14:57:00 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+// Define the employee variable
+const employee = {
+  name: "John Doe",
+  streetAddress: "123 Main St",
+};
+
+// Function to update employee with key and value
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value,
+  };
+}
+
+// Function to destructively update employee with key and value
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// Function to delete property from employee by key
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// Function to destructively delete property from employee by key
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
